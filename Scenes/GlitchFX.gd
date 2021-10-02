b21d325b1c8addceac732f71bad5548ac52f629b
mr-yak <41585@kristin.school.nz>
2021-10-02 20:04:27 +1300

    glitches

--- Scenes/GlitchFX.gd
+++ Scenes/GlitchFX.gd
@@ -1,16 +1,26 @@
+tool
+
 extends Control
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+	
+func invert(set):
+	get_node("Invert").visible = set
 
+func wirl(set):
+	get_node("Wirl").visible = set
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func normalise(set):
+	get_node("Normalised").visible = set
+
+func deepfried(set):
+	get_node("Deep Fried").visible = set
+
+func wave(set):
+	get_node("Wave").visible = set
+
+func old(set):
+	get_node("Ye Olde").visible = set
