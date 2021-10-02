fbdc528ae241716cb36bca0c9f7780743cd7c184
Jel2217 <54785779+Jel2217@users.noreply.github.com>
2021-10-02 15:25:05 +1300

    nope

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -1,25 +1,23 @@
 extends KinematicBody2D
 
-
+var move_speed = 100
 var speed = 100
 onready var nav =  get_node("/root/Level/Navigation2D")
 onready var player =  get_node("/root/Level/Player")
-var path := PoolVector2Array()
+var path
+var points
+var points_index = 0
+var velocity = Vector2.ZERO
+
 
 func _physics_process(delta):
+	print(points_index)
 	if Input.is_action_pressed("test"):
-		path = nav.get_simple_path(player.position, position)
-	# Calculate the movement distance for this frame
-	var distance_to_walk = speed * delta
-	# Move the player along the path until he has run out of movement or the path ends.
-	while distance_to_walk > 0 and path.size() > 0:
-		var distance_to_next_point = position.distance_to(path[0])
-		if distance_to_walk <= distance_to_next_point:
-			# The player does not have enough movement left to get to the next point.
-			position += position.direction_to(path[0]) * distance_to_walk
-		else:
-			# The player get to the next point
-			position = path[0]
-			path.remove(0)
-		# Update the distance to walk
-		distance_to_walk -= distance_to_next_point
+		print("y")
+		points = nav.get_simple_path(player.position, position)
+	if !points: return
+	var target = points[points_index]
+	if position.distance_to(target) < 1:
+		target = points[points_index + 1]
+	velocity = (target - position).normalized() * move_speed
+	velocity = move_and_slide(velocity)
