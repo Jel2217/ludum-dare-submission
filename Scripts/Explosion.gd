b05c6f7e2c5727a290e2ff14a35166a1c51f1e72
mr-yak <41585@kristin.school.nz>
2021-10-03 11:59:23 +1300

    yay

--- Scripts/Explosion.gd
+++ Scripts/Explosion.gd
@@ -8,3 +8,9 @@ func start():
 
 func _on_Timer_timeout():
 	queue_free()
+
+
+func _on_Area2D_body_entered(body):
+	if body.has_method("update_health"):
+		var new_health = body.health-20
+		body.update_health(new_health)
