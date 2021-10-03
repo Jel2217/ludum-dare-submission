b201eaddd24a65231e02fcc50eadb7e4dc64ef18
Jel2217 <54785779+Jel2217@users.noreply.github.com>
2021-10-04 11:36:18 +1300

    s

--- Scripts/Spawner.gd
+++ Scripts/Spawner.gd
@@ -7,12 +7,12 @@ var slime_purple = preload("res://Scenes/Enemies/PurpleSlime.tscn")
 var slime_red = preload("res://Scenes/Enemies/RedSlime.tscn")
 var slime_yellow = preload("res://Scenes/Enemies/YellowSlime.tscn")
 
-export var waves = 3
-export var blue_slime_amount = 3
-export var green_slime_amount = 1
-export var purple_slime_amount = 0
-export var red_slime_amount = 1
-export var yellow_slime_amount = 1
+export var waves : int
+export var blue_slime_amount : int
+export var green_slime_amount : int
+export var purple_slime_amount : int
+export var red_slime_amount : int 
+export var yellow_slime_amount : int
 
 var enemy_count = blue_slime_amount + green_slime_amount + purple_slime_amount + red_slime_amount + yellow_slime_amount
 var enemy_quota # Every enemy player kills will increase quota - quota == count means wave is over
@@ -21,19 +21,21 @@ onready var block_tile_map = $"../BlockTileMap"
 var player_in_room = false
 var current_wave = 0
 var rng = RandomNumberGenerator.new()
-
+var finished = false
 func _ready():
 	rng.randomize()
 	
 	set_block_tile_map(false)
 
 func _process(delta):
+	if finished: return
 	if player_in_room:
 		if not get_tree().get_nodes_in_group("enemy"):
 			current_wave += 1
 			if current_wave > waves:
 				player_in_room = false
 				set_block_tile_map(false)
+				current_wave = 0
 			else:
 				for i in blue_slime_amount:
 					spawn_enemy(slime_blue)
@@ -63,5 +65,3 @@ func spawn_enemy(enemy):
 	$"../".add_child(i)
 	i.position = Vector2(rng.randi_range(position.x, position.x + $CollisionShape2D.position.x * 2),  
 	rng.randi_range(position.y, position.y + $CollisionShape2D.position.y * 2)) # Essentially set the position to a random range within the radius of the collision box
-	
-	
