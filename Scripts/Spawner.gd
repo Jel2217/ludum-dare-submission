56c65987ece46863d47ce12eb1ad0395caad6bfe
Jel2217 <54785779+Jel2217@users.noreply.github.com>
2021-10-04 09:43:40 +1300

    spawner

--- Scripts/Spawner.gd
+++ Scripts/Spawner.gd
@@ -7,25 +7,44 @@ var slime_purple = preload("res://Scenes/Enemies/PurpleSlime.tscn")
 var slime_red = preload("res://Scenes/Enemies/RedSlime.tscn")
 var slime_yellow = preload("res://Scenes/Enemies/YellowSlime.tscn")
 
-export var waves = 1
-export var blue_slime_amount = 0
-export var green_slime_aomunt = 0
-export var purple_slime_aomunt = 0
-export var red_slime_aomunt = 0
-export var yellow_slime_aomunt = 0
+export var waves = 3
+export var blue_slime_amount = 3
+export var green_slime_amount = 1
+export var purple_slime_amount = 0
+export var red_slime_amount = 1
+export var yellow_slime_amount = 1
 
-var enemy_count = blue_slime_amount + green_slime_aomunt + purple_slime_aomunt + red_slime_aomunt + yellow_slime_aomunt
+var enemy_count = blue_slime_amount + green_slime_amount + purple_slime_amount + red_slime_amount + yellow_slime_amount
 var enemy_quota # Every enemy player kills will increase quota - quota == count means wave is over
 
 onready var block_tile_map = $"../BlockTileMap"
 var player_in_room = false
-
+var current_wave = 0
 var rng = RandomNumberGenerator.new()
 
 func _ready():
 	rng.randomize()
 	
 	set_block_tile_map(false)
+
+func _process(delta):
+	if player_in_room:
+		if not get_tree().get_nodes_in_group("enemy"):
+			current_wave += 1
+			if current_wave > waves:
+				player_in_room = false
+				set_block_tile_map(false)
+			else:
+				for i in blue_slime_amount:
+					spawn_enemy(slime_blue)
+				for i in green_slime_amount:
+					spawn_enemy(slime_green)
+				for i in purple_slime_amount:
+					spawn_enemy(slime_purple)
+				for i in red_slime_amount:
+					spawn_enemy(slime_red)
+				for i in yellow_slime_amount:
+					spawn_enemy(slime_yellow)
 
 func _on_Spawner_body_entered(body):
 	if body.is_in_group("player"):
@@ -38,9 +57,9 @@ func set_block_tile_map(active):
 	if active: block_tile_map.show()
 	else: block_tile_map.hide()
 
-func _physics_process(delta):
+func spawn_enemy(enemy):
 	if not player_in_room: return
-	var i = slime_blue.instance()
+	var i = enemy.instance()
 	$"../".add_child(i)
 	i.position = Vector2(rng.randi_range(position.x, position.x + $CollisionShape2D.position.x * 2),  
 	rng.randi_range(position.y, position.y + $CollisionShape2D.position.y * 2)) # Essentially set the position to a random range within the radius of the collision box
