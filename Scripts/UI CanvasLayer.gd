b60c49ffa5ce7127e7a47e3ddee5a24ff6d387e4
mr-yak <41585@kristin.school.nz>
2021-10-04 15:05:52 +1300

    Added more bugs

--- Scripts/UI CanvasLayer.gd
+++ Scripts/UI CanvasLayer.gd
@@ -3,6 +3,7 @@ extends CanvasLayer
 var full_heart = preload("res://Images/UI/Hearts/heart1.png")
 var half_heart = preload("res://Images/UI/Hearts/heart2.png")
 var empty_heart = preload("res://Images/UI/Hearts/heart3.png")
+var level_file = "user://level.save"
 onready var heart_hbox = $"Heart Hbox"
 
 func update_healthbar(value):
@@ -13,14 +14,32 @@ func update_healthbar(value):
 			heart_hbox.get_child(i).texture = half_heart
 		else:
 			heart_hbox.get_child(i).texture = empty_heart
-		print(i)
 
 
 func _on_Player_health(value):
 	update_healthbar(value)
 
 func player_won():
+	if load_score() == get_parent().level:
+		save_level(get_parent().level+1)
 	print("player won")
 
 func player_lost():
 	print("player lost")
+	
+func save_level(level):
+	var file = File.new()
+	file.open(level_file, File.WRITE)
+	file.store_var(level)
+	file.close()
+	
+func load_score():
+	var file = File.new()
+	if file.file_exists(level_file):
+		file.open(level_file, File.READ)
+		var level = file.get_var()
+		file.close()
+		return level
+	else:
+		var level = 1
+		return level
